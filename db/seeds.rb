e8c858d6f9126a9ff83c5279dc42260afe979266
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-05-20 13:47:35 -0400

    still having db migration troubles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create!(name: ‘David Dogg’, username: ‘DaDog33’, encrypted_password: ‘password1’, location: ‘New York City’, department ‘sales’, position: ‘manager’, active: ‘true’, aboutMe: ‘dog lover, guitarist, writer’)
-User.create!(name: ‘Carly Catt’, username: ‘CaCatt44’, encrypted_password: ‘password2’, location: ‘New York City’, department ‘sales’, position: ‘representative’, active: ‘true’, aboutMe: ‘cat lover, painter, amateur cook’)
-User.create!(name: ‘Freddy Fish’, username: ‘FrFish44’, encrypted_password: ‘password3’, location: ‘San Fransisco’, department ‘sales’, position: ‘manager’, active: ‘true’, aboutMe: ‘runner, Harry Potter fan’)
-User.create!(name: ‘Harry Hamster’, username: ‘HaHam55’, encrypted_password: ‘password4’, location: ‘Los Angeles’, department ‘sales’, position: ‘representative’, active: ‘true’, aboutMe: ‘vegan, yoga fanatic, ballroom dancer’)
+User.create!(name: 'David Dogg', username: 'DDog33', encrypted_password: 'password1' , location: 'New York City', 
+            department: 'sales', position: 'manager', active: true, aboutMe: 'dog lover, guitarist, writer')
+
+User.create!(name: 'Carly Catt', username: 'CCatt44', encrypted_password: 'password2', location: 'New York City', 
+            department: 'sales', position: 'representative', active: true, aboutMe: 'cat lover, painter, amateur cook')
+
+User.create!(name: 'Freddy Fish', username: 'FFish44', encrypted_password: 'password3', location: 'San Fransisco',
+             department: 'sales', position: 'manager', active: true, aboutMe:'runner, Harry Potter fan')
+
+User.create!(name: 'Harry Hamster', username: 'HHam55', encrypted_password: 'password4', location: 'Los Angeles',
+             department: 'sales', position: 'representative', active: true, aboutMe: 'vegan, yoga fanatic, ballroom dancer')
+             
 puts "#{User.count} users created!"
 
-@user = User.create! (name: ‘David Dogg’, username: ‘DaDog33’, encrypted_password: ‘password1’, location: ‘New York City’, department ‘sales’, position: ‘manager’, active: ‘true’, aboutMe: ‘dog lover, guitarist, writer’)
-@post1 = @user.posts.build (projectTitle:'Fire Hydrant',  projectDesc 'Building FH app' imageUrl 'www.google.com'
+@user = User.create! (name: 'David Dogg', username: 'DDog33', encrypted_password: 'password1', location: 'New York City', 
+                      department 'sales', position: 'manager', active: ‘true’, aboutMe: 'dog lover, guitarist, writer')
+@post1 = @user.posts.build (projectTitle:'Fire Hydrant',  projectDesc 'Building FH app' imageUrl 'www.google.com') 
 @post1.save   
 puts "#{Post.count} posts created!"
